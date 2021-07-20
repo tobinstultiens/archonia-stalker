00f7264d1cbb8c2713ebf6d16e4b9d5c885c1923
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-07-21 00:29:41 +0200

    Added mail sender now only need to resolve the configurations

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import scrapy
 import yaml
 from scrapy.http.request.form import FormRequest
+from scrapy.mail import MailSender
 
 config = yaml.safe_load(open("settings.yaml"))
+mailer = MailSender()
 
 
 class BlogSpider(scrapy.Spider):
@@ -25,7 +27,13 @@ class BlogSpider(scrapy.Spider):
         ]
 
     def parse2(self, response):
-        for title in response.xpath(
-            '//*[@id="main-content"]/div/div[2]/div[2]/div/div/div/div/h4/a'
-        ):
-            yield {"title": title.css("::text").get()}
+        for row in response.xpath('//*[@id="main-content"]/div/div[2]/div[2]/div'):
+            title = row.css("a::text").get()
+            stock = row.css("span::text").get()
+            if stock != "Out of stock":
+                mailer.send(to=[config["EMAIL"]], subject=f"{title} is now in stock", body=config["WISHLIST"])
+
+            yield {
+                "title": title,
+                "stock": stock
+            }
