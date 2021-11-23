0cb0e03ff7342d2b800238bfb08227c333bae2ea
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-11-23 01:16:24 +0100

    Added title

--- volumes.py
+++ volumes.py
@@ -15,8 +15,9 @@ config = yaml.safe_load(open(settings))
 
 
 class WishListItem:
-    def __init__(self, title, url, last_checked):
+    def __init__(self, title, number, url, last_checked):
         self.title = title
+        self.number = number
         self.url = url
         self.last_checked = last_checked
 
@@ -46,27 +47,29 @@ class ArchoniaWishListSpider(scrapy.Spider):
         return (Request(url, callback=self.parse_volume) for url in self.tracked_items)
 
     def parse_volume(self, response):
+        title = response.xpath(
+            '//*[@id="main-content"]/div/div[5]/div/table/tbody/tr[4]/td[2]/a').css("a::text").get()
         box = response.xpath(
             '//*[@id="main-content"]/div/div[4]/div[1]/div[1]/a')[-1]
-        volume = self.parse_wishlist_item(box)
-        if int(volume.title) != int(self.tracked_items[response.url]):
-            self.tracked_items[response.url] = int(volume.title)
+        volume = self.parse_wishlist_item(box, title)
+        if int(volume.number) != int(self.tracked_items[response.url]):
+            self.tracked_items[response.url] = int(volume.number)
             self.publish_discord_notification(volume)
         self.update_tracked_items()
 
-    def parse_wishlist_item(self, row):
-        title = row.css("span::text").get()
+    def parse_wishlist_item(self, row, title):
+        number = row.css("span::text").get()
         url = row.css("a::attr(href)").get()
-        return WishListItem(title, url, datetime.now())
+        return WishListItem(title, number, url, datetime.now())
 
     def publish_discord_notification(self, wishlist_item: WishListItem):
         webhook = DiscordWebhook(
             url=config["WEBHOOK_URL"],
-            content=f'{wishlist_item.title} {config["DISCORD_MENTION"]}',
+            content=f'{wishlist_item.title} {wishlist_item.number} {config["DISCORD_MENTION"]}',
         )
         embed = DiscordEmbed(
-            title=f"{wishlist_item.title}",
-            description="is in stock!",
+            title=f"{wishlist_item.title} vol {wishlist_item.number}",
+            description="is available!",
             color="03b2f8",
         )
         embed.set_url(url=f"https://www.archonia.com{wishlist_item.url}")
